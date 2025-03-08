7014b2c8491530affb62cc807d9c86e9e3fdcfdc
DraymeM <mixcrafting.d@gmail.com>
2025-03-08 16:45:08 +0100

    added preview for pictures

--- obj/Debug/net8.0-windows/Insect-Betyar-Admin-App.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Insect-Betyar-Admin-App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insect-Betyar-Admin-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755d9332661e35db1878f258deec38c164ff1af9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2843b2435ce6d920bd70f438338a19724f797fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insect-Betyar-Admin-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insect-Betyar-Admin-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
